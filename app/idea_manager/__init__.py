9066acd16e45cea7ac56744f4a8e721426ccfc93
peach lasagna <59787538+peach-lasagna@users.noreply.github.com>
2020-07-29 14:11:04 +0700

    Update and rename idea_manager/__init__.py to app/idea_manager/__init__.py

---
+++ app/idea_manager/__init__.py
@@ -0,0 +1,4 @@
+from idea_manager.todo import main as todo_main
+from idea_manager.ideas import IdeaView, IdeaModel
+
+__all__ = ["IdeaView", "IdeaModel", "todo_main"]
--- idea_manager/__init__.py
+++
@@ -1,4 +0,0 @@
-from .todo import main as todo_main
-from .ideas import IdeaView, IdeaModel
-
-__all__ = ["IdeaView", "IdeaModel", "todo_main"]
